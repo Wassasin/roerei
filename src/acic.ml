0ddb9e399da565c42c5cf6ba3a1e85612137196e
Wouter Geraedts <git@woutergeraedts.nl>
2015-03-01 21:51:46 +0100

    Added first attempt at acic inclusion

---
+++ src/acic.ml
@@ -0,0 +1,113 @@
+(* Original file as provided by the HELM Project under the LGPL 2.1 license *)
+(* See coq/plugins/xml/acic.ml *)
+
+type existential_key = int  (* from Term *)
+type identifier = string  (* from Names *)
+
+type id = string  (* the type of the (annotated) node identifiers *)
+type uri = string
+
+(* Names *)
+type module_ident = identifier
+type dir_path = module_ident list
+type name = Name of identifier | Anonymous
+
+(* Universe from Univ *)
+module UniverseLevel = struct
+  type t =
+    | Set
+    | Level of dir_path * int
+end
+
+type universe =
+  | Atom of UniverseLevel.t
+  | Max of UniverseLevel.t list * UniverseLevel.t list
+
+(* Sorts from Term *)
+type contents = Pos | Null
+
+type sorts =
+  | Prop of contents                      (* proposition types *)
+  | Type of universe
+
+type 'constr context_entry =
+   Decl of 'constr             (* Declaration *)
+ | Def  of 'constr * 'constr   (* Definition; the second argument (the type) *)
+                               (* is not present in the DTD, but is needed   *)
+                               (* to use Coq functions during exportation.   *)
+
+type 'constr hypothesis = identifier * 'constr context_entry
+
+(* Possibly needed at a later moment *)
+(* type context = constr hypothesis list *)
+(* type conjecture = existential_key * context * constr *)
+(* type metasenv = conjecture list *)
+
+(* list of couples section path -- variables defined in that section *)
+type params = (string * uri list) list
+
+(*
+type obj =
+   Constant of string *                            (* id,           *)
+    constr option * constr *                       (*  value, type, *)
+    params                                         (*  parameters   *)
+ | Variable of
+    string * constr option * constr *              (* name, body, type *)
+    params                                         (*  parameters   *)
+ | CurrentProof of
+    string * metasenv *                            (*  name, conjectures, *)
+    constr * constr                                (*  value, type        *)
+ | InductiveDefinition of
+    inductiveType list *                           (* inductive types ,      *)
+    params * int                                   (*  parameters,n ind. pars*)
+and inductiveType =
+ identifier * bool * constr *                 (* typename, inductive, arity *)
+  constructor list                            (*  constructors              *)
+and constructor =
+ identifier * constr                          (* id, type *)
+*)
+
+type aconstr =
+  | ARel       of id * int * id * identifier
+  | AVar       of id * uri
+  | AEvar      of id * existential_key * aconstr list
+  | ASort      of id * sorts
+  | ACast      of id * aconstr * aconstr
+  | AProds     of (id * name * aconstr) list * aconstr
+  | ALambdas   of (id * name * aconstr) list * aconstr
+  | ALetIns    of (id * name * aconstr) list * aconstr
+  | AApp       of id * aconstr list
+  | AConst     of id * explicit_named_substitution * uri
+  | AInd       of id * explicit_named_substitution * uri * int
+  | AConstruct of id * explicit_named_substitution * uri * int * int
+  | ACase      of id * uri * int * aconstr * aconstr * aconstr list
+  | AFix       of id * int * ainductivefun list
+  | ACoFix     of id * int * acoinductivefun list
+and ainductivefun =
+ id * identifier * int * aconstr * aconstr
+and acoinductivefun =
+ id * identifier * aconstr * aconstr
+and explicit_named_substitution = id option * (uri * aconstr) list
+
+type acontext = (id * aconstr hypothesis) list
+type aconjecture = id * existential_key * acontext * aconstr
+type ametasenv = aconjecture list
+
+type aobj =
+   AConstant of id * string *                      (* id,           *)
+    aconstr option * aconstr *                     (*  value, type, *)
+    params                                         (*  parameters   *)
+ | AVariable of id *
+    string * aconstr option * aconstr *            (* name, body, type *)
+    params                                         (*  parameters   *)
+ | ACurrentProof of id *
+    string * ametasenv *                           (*  name, conjectures, *)
+    aconstr * aconstr                              (*  value, type        *)
+ | AInductiveDefinition of id *
+    anninductiveType list *                        (* inductive types ,      *)
+    params * int                                   (*  parameters,n ind. pars*)
+and anninductiveType =
+ id * identifier * bool * aconstr *           (* typename, inductive, arity *)
+  annconstructor list                         (*  constructors              *)
+and annconstructor =
+ identifier * aconstr                         (* id, type *)
