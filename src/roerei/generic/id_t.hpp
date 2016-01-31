b66d19f31c622527fc13099e660e77314e011006
Wouter Geraedts <git@woutergeraedts.nl>
2016-01-31 20:29:34 +0100

    Fixed id_t conversion rules

--- src/roerei/generic/id_t.hpp
+++ src/roerei/generic/id_t.hpp
@@ -3,47 +3,49 @@
 namespace roerei
 {
 
+template<typename T>
 class id_t
 {
 	size_t id;
 
-public:
+protected:
 	id_t(size_t _id)
 		: id(_id)
 	{}
 
+public:
 	size_t unseal() const
 	{
 		return id;
 	}
 
-	bool operator==(id_t const rhs) const
+	bool operator==(T const rhs) const
 	{
 		return id == rhs.id;
 	}
 
-	bool operator<(id_t const rhs) const
+	bool operator<(T const rhs) const
 	{
 		return id < rhs.id;
 	}
 
-	bool operator>=(id_t const rhs) const
+	bool operator>=(T const rhs) const
 	{
 		return !operator<(rhs);
 	}
 };
 
-struct object_id_t : public id_t
+struct object_id_t : public id_t<object_id_t>
 {
 	object_id_t(size_t _id) : id_t(_id) {}
 };
 
-struct feature_id_t : public id_t
+struct feature_id_t : public id_t<feature_id_t>
 {
 	feature_id_t(size_t _id) : id_t(_id) {}
 };
 
-struct dependency_id_t : public id_t
+struct dependency_id_t : public id_t<dependency_id_t>
 {
 	dependency_id_t(size_t _id) : id_t(_id) {}
 };
