c59db8504f43ee70623d0789436ba89a111a79e1
Wouter Geraedts <wouter@tweedegolf.com>
2017-06-09 19:35:59 +0200

    WIP JIT

--- src/roerei/generic/set_operations.hpp
+++ src/roerei/generic/set_operations.hpp
@@ -4,6 +4,31 @@
 
 namespace roerei {
 
+template<typename A, typename B, typename A_F, typename B_F>
+bool set_compute_does_intersect(
+		A const& a_begin, A const& a_end,
+		B const& b_begin, B const& b_end,
+		A_F const& a_f,
+		B_F const& b_f
+) {
+	auto a_it = a_begin;
+	auto b_it = b_begin;
+
+	while (a_it != a_end && b_it != b_end) {
+		auto ai = a_f(*a_it);
+		auto bi = b_f(*b_it);
+		if (ai < bi) {
+			a_it++;
+		} else if(bi < ai) {
+			b_it++;
+		} else { // thus a == b
+			return true;
+		}
+	}
+
+	return false;
+}
+
 template<typename A, typename B, typename A_F, typename B_F, typename F>
 void set_compute_intersect(
 		A const& a_begin, A const& a_end,
