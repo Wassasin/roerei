1b0241736b4b66d82e2f7bae4d2dcadf616b38e8
Wouter Geraedts <wouter@tweedegolf.com>
2017-10-09 15:46:49 +0200

    Improved on graph generator

--- src/roerei/structure_exporter.cpp
+++ src/roerei/structure_exporter.cpp
@@ -9,10 +9,17 @@
 #include <roerei/storage.hpp>
 
 #include <roerei/structure_exporter.hpp>
+#include <roerei/generic/sparse_unit_matrix.hpp>
+#include <roerei/generic/id_t.hpp>
 
 namespace roerei {
   typedef std::vector<std::string> subgroup_t;
   typedef std::pair<subgroup_t, subgroup_t> edge_key_t;
+
+  struct subgroup_id_t : public id_t<subgroup_id_t>
+  {
+    subgroup_id_t(size_t _id) : id_t(_id) {}
+  };
 
   struct synopsis_t {
     size_t count;
@@ -41,7 +48,7 @@ namespace roerei {
   }
 
   inline bool is_too_small(subgroup_t const& group, synopsis_t const& s) {
-    return group.size() > 1 && s.count < 200;
+    return group.size() > 0 && s.count < 500;
   }
 
   inline void print_subgroup(std::ostream& os, subgroup_t const& group) {
@@ -66,20 +73,21 @@ namespace roerei {
   {
     std::ofstream os(output_path+"/dependencies.dot");
 
-    os << "digraph dependencies {" << std::endl;
+    os << "graph dependencies {" << std::endl;
+
     os << "model=\"subset\";" << std::endl;
-    os << "remincross=\"true\";" << std::endl;
+    os << "splines=\"true\";" << std::endl;
     os << "size=\"16.4, 10.7\";" << std::endl;
     os << "overlap=false;" << std::endl;
-    os << "ratio=fill;" << std::endl;
+    os << "rankdir=\"LR\";" << std::endl;
     os << "root=\"Coq\";" << std::endl;
 
     auto corpii = {
         "Coq.frequency",
-        "ch2o.frequency",
+        /*"ch2o.frequency",
         "CoRN.frequency",
         "MathClasses.frequency",
-        "mathcomp.frequency"
+        "mathcomp.frequency"*/
     };
 
     std::map<subgroup_t, synopsis_t> result;
@@ -95,7 +103,31 @@ namespace roerei {
         }
     }
 
-    size_t i = 0;
+    size_t max_depth = std::numeric_limits<size_t>::min();
+    for (auto const& kvp : result) {
+      max_depth = std::max(max_depth, kvp.first.size());
+    }
+
+    for (size_t i = max_depth; i > 0; i--) {
+      std::cerr << i << std::endl;
+      auto const result_hardcopy = result; // Immutable copy to operate from
+      for (auto const& kvp : result_hardcopy) {
+        if (kvp.first.size() != i) {
+          continue;
+        }
+
+        if (!is_too_small(kvp.first, kvp.second)) {
+          continue;
+        }
+
+        auto us = kvp.first; // Make a copy;
+        us.pop_back();
+        result[us] += kvp.second;
+        result.erase(kvp.first);
+      }
+    }
+
+    /*size_t i = 0;
     while(std::any_of(result.begin(), result.end(), [](auto const& kvp) {
       return is_too_small(kvp.first, kvp.second);
     })) {
@@ -113,11 +145,22 @@ namespace roerei {
         result[us] += kvp.second;
         result.erase(kvp.first);
       }
-    }
+    }*/
 
+    encapsulated_vector<subgroup_id_t, subgroup_t> subgroups;
+    for (auto const& kvp : result) {
+      subgroups.emplace_back(kvp.first);
+    }
+    std::map<subgroup_t, subgroup_id_t> subgroup_map;
+    subgroups.iterate([&](subgroup_id_t id, subgroup_t const& g) {
+      subgroup_map.emplace(g, id);
+    });
+
+    sparse_unit_matrix_t<subgroup_id_t, subgroup_id_t> edge_matrix(subgroups.size(), subgroups.size());
     std::map<edge_key_t, float> edges;
+
     auto find_largest_f = [&](subgroup_t g) { // Copy
-      while (g.size() > 1) {
+      while (g.size() > 0) {
         if (result.find(g) != result.end()) {
           break;
         }
@@ -132,36 +175,83 @@ namespace roerei {
       d.dependency_matrix.citerate([&](auto const& row) {
         subgroup_t from = find_largest_f(create_subgroup(d.objects[row.row_i]));
         for(auto const& kvp : row) {
-          subgroup_t to = find_largest_f(create_subgroup(d.dependencies[kvp.first]));
-          edges[std::make_pair(to, from)] += kvp.second;
+          subgroup_t fromNew = from;
+          subgroup_t toNew = find_largest_f(create_subgroup(d.dependencies[kvp.first]));;
+
+          subgroup_id_t x(subgroup_map.at(fromNew));
+          subgroup_id_t y(subgroup_map.at(toNew));
+
+          if (x.unseal() > y.unseal()) {
+            std::swap(x, y);
+            std::swap(fromNew, toNew);
+          }
+
+          edges[std::make_pair(fromNew, toNew)] += kvp.second;
+          edge_matrix.set(std::make_pair(x, y));
         }
       });
     }
+
+    edge_matrix.transitive();
+    edge_matrix.transpose();
 
     std::vector<subgroup_t> gs;
     for(auto const& kvp : result) {
       gs.emplace_back(kvp.first);
     }
+
     std::stable_sort(gs.begin(), gs.end(), [&](subgroup_t const& x, subgroup_t const& y) {
-      return edges.find(std::make_pair(x, y)) == edges.end();
+      return !edge_matrix[std::make_pair(subgroup_map.at(x), subgroup_map.at(y))];
     });
 
-    for(auto const& kvp : result) {
-      os << "\"";
-      print_subgroup(os, kvp.first);
-      os << "\";" << std::endl;
-    }
+    auto should_parse_f = [](subgroup_t const& g) {
+      return (!g.empty() && g[0] == "Coq");
+    };
 
-    for(auto const& kvp : edges) {
-      if (kvp.first.first == kvp.first.second || kvp.second == 0.0f) {
+    auto should_parse_pair_f = [&](subgroup_t const& x, subgroup_t const& y) {
+      return !(x == y || !should_parse_f(x) || !should_parse_f(y));
+    };
+
+    for(auto const& g : gs) {
+      if (!should_parse_f(g)) {
         continue;
       }
 
       os << "\"";
-      print_subgroup(os, kvp.first.first);
-      os << "\" -> \"";
-      print_subgroup(os, kvp.first.second);
+      print_subgroup(os, g);
       os << "\";" << std::endl;
+    }
+
+    float max_edge_width = std::numeric_limits<float>::min();
+    for(auto const& kvp : edges) {
+      if (!should_parse_pair_f(kvp.first.first, kvp.first.second)) {
+        continue;
+      }
+      max_edge_width = std::max(max_edge_width, kvp.second);
+    }
+
+    auto transform_range_f = [](float value, float max, float desired_max) {
+      return (value / max) * desired_max;
+    };
+    auto cap_f = [](float x, float factor) {
+      return std::ceil(x*factor)/factor;
+    };
+
+    for(auto const& kvp : edges) {
+      if (!should_parse_pair_f(kvp.first.first, kvp.first.second) || kvp.second == 0.0f) {
+        continue;
+      }
+
+      std::cout << kvp.second << std::endl;
+
+      os << "\"";
+      print_subgroup(os, kvp.first.first);
+      os << "\" -- \"";
+      print_subgroup(os, kvp.first.second);
+      os << "\" [weight=" << kvp.second
+        << ", len=" << cap_f(transform_range_f(1.0f/kvp.second, 1.0f, 20.0f), 10.0f)
+        << ", penwidth=" << cap_f(transform_range_f(kvp.second, max_edge_width, 5.0f), 1.0f)
+        << "];" << std::endl;
     }
     os << "}" << std::endl;
   }
